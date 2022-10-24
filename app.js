edeca9b317d91d2ecf77d5b9734405bad6a11ef3
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-10-24 11:57:07 -0400

    completed inro assignment

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Julia";
+let lastName;
+var age;
 
 // Exercise 2
+lastName = "Griffiths Allison"
+age = 22;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//4)
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
